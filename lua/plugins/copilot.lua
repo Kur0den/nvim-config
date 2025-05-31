ef5d9afba8dcc5d69d83f095babd319df1fd81e4
Kur0den <git@kur0den.net>
2025-06-01 03:21:01 +0900

    change copilot suggesion key

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,15 @@ return {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    require("copilot").setup({})
+    require("copilot").setup({
+      suggestion = {
+        keymap = {
+          accept = "<M-l>L",
+          accept_word = "<M-l>w",
+          accept_line = "<M-l>l",
+        }
+      }
+    })
   end,
 }
 
