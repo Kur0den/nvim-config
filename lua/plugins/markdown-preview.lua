a406e491a928aa4e583cfa8a6514cfa7ba8f3332
Kur0den <git@kur0den.net>
2025-06-03 23:27:25 +0900

    markdown-previewの設定をちょっと変更

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,6 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && pnpm install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
+  build = function() vim.fn["mkdp#util#install"]() end,
   ft = { "markdown" },
 }
