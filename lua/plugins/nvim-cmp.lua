012c25164821becf109d3619b8b912231c8b997f
Kur0den <git@kur0den.net>
2025-06-06 14:24:02 +0900

    add nvim-autopairs

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -105,6 +105,13 @@ return {
       -- lspconfig.tsserver.setup {
       --   capabilities = capabilities
       -- }
+      --
+      -- autopairs
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
     end
   },
 }
