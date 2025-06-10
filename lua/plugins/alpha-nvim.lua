27d5ce96d45908e714c58dc18835603c3b8caf3f
kur0den <git@kur0den.net>
2025-06-10 10:51:58 +0900

    alphaの設定を変更

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -1,6 +1,20 @@
 return {
   'goolord/alpha-nvim',
   config = function ()
-    require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    local alpha = require('alpha')
+    local dashboard = require('alpha.themes.dashboard')
+
+    dashboard.section.header.val = {
+      "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+      "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+      "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+      "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+      "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+      "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+      "",
+      "                   にゃーん！"
+    }
+
+    alpha.setup(dashboard.config)
   end
 };
