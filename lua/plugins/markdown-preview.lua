4804ca26447d8f564c9cf5ee69ed571b730855e3
Kur0den <git@kur0den.net>
2025-06-12 20:06:08 +0900

    mdプレビューのプラグインを変更

--- lua/plugins/markdown-preview.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = function() vim.fn["mkdp#util#install"]() end,
-  ft = { "markdown" },
-}
