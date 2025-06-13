d0de309309417fd4ebb0f6837a76f7837f44a6f7
Kur0den <git@kur0den.net>
2025-06-14 02:46:13 +0900

    treesitterの設定を変更

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,5 +2,26 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = 'master',
   lazy = false,
-  build = ":TSUpdate"
+  build = ":TSUpdate",
+  opts = {
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    ensure_installed = {
+      "python",
+      "lua",
+      "rust",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "json",
+      "markdown",
+      "markdown_inline",
+    },
+
+  }
 }
