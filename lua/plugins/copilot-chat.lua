4d636d1972e121a364638304fe36a27813e14778
kur0den <git@kur0den.net>
2025-06-19 01:35:09 +0900

    copilotchatに遅延読み込みを追加
    おもい

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -5,6 +5,7 @@ return {
       { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
+    event = "insertEnter",
     -- build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
