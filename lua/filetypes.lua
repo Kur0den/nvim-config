733ce035c84415282547f6d60ab08fc3a59c934a
Kur0den <git@kur0den.net>
2025-06-20 21:51:57 +0900

    compose.ymlのfiletypeをyaml.composeにするように
    lspがうごくように

---
+++ lua/filetypes.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = {"docker-compose*.yml", "compose*.yml"},
+  callback = function()
+    vim.bo.filetype = "yaml.docker-compose"
+  end,
+  desc = "Set filetype to yaml.docker-compose for docker-compose*.yml and compose*.yml files"
+})
