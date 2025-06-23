1b45d6ca22afbe324ae8003e0b7eeb3fb8001a19
Kur0den <git@kur0den.net>
2025-06-23 18:21:39 +0900

    compose*.yaml, docker-compose*.yamlをyaml.docker-composeにするように

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {"docker-compose*.yml", "compose*.yml"},
+  pattern = {"docker-compose*.yml", "docker-compose*.yaml", "compose*.yml", "compose*.yaml"},
   callback = function()
     vim.bo.filetype = "yaml.docker-compose"
   end,
