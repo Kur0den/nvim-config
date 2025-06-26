012cb34e97d72e2a2f0209cbd7570479e278491a
Kur0den <git@kur0den.net>
2025-06-26 19:45:28 +0900

    オプションの記法を変更

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,17 +2,18 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<M-l>L",
-          accept_word = "<M-l>w",
-          accept_line = "<M-l>l",
-        }
+  opts = {
+    suggestion = {
+      filetypes = {
+        markdown = false,
+      },
+      auto_trigger = true,
+      keymap = {
+        accept = "<M-l>L",
+        accept_word = "<M-l>w",
+        accept_line = "<M-l>l",
       }
-    })
-  end,
+    }
+  }
 }
 
