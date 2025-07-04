45d17889a3afb9810c51cc001d3331ad668b99d6
Kur0den <git@kur0den.net>
2025-07-04 12:26:11 +0900

    change dial.lua settings

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -1,8 +1,26 @@
 return {
   "monaqa/dial.nvim",
-    config = function()
-      local key = vim.keymap
-    -- 提示されたキーマップの設定
+  config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group{
+      default = {
+        augend.integer.alias.decimal_int,
+        augend.integer.alias.hex,
+        augend.constant.alias.bool,
+        augend.date.alias["%Y-%m-%d"],
+        augend.date.alias["%Y/%m/%d"],
+        augend.date.alias["%Y年%-m月%-d日"],
+        augend.date.alias["%m/%d"],
+        augend.date.alias["%H:%M"],
+        augend.date.alias.ja_weekday_full,
+        -- nazeka ugokanai
+        -- augend.hexcolor.new{
+        --   case = "upper", -- or "lower", "prefer_upper", "prefer_lower", see below
+        -- }
+      }
+    }
+    local key = vim.keymap
+    -- 提示されたキーマップの設定    augend.hexcolor.new{
     key.set("n", "<C-a>", function()
       require("dial.map").manipulate("increment", "normal")
     end, { silent = true }) 
