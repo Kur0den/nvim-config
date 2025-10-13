93aa277a51d79a3f7979c30a62ea4325225f4532
Kur0den <git@kur0den.net>
2025-10-13 18:49:54 +0900

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,53 @@ return {
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
     },
+    lualine = {
+      lualine_c = {
+        {
+          "filename",
+          symbols = {
+            modified="*",
+            readonly="[R]",
+          }
+        },
+        "filesize",
+      },
+      lualine_x = {
+        {
+          "encoding",
+          showbomb=true,
+        },
+        "fileformat",
+        "filetype",
+      }
+    },
+    tabline = {
+      lualine_a = {
+        {
+          'tabs',
+          mode=2,
+          path=0,
+          use_mode_colors=true,
+          symbols = {
+            modified="*",
+          }
+        }
+      },
+      lualine_z = {
+        {
+          "buffers",
+          mode=2,
+          use_mode_colors=true,
+          filetype_names = {
+            oil="Oil",
+            TelescopePrompt="Telescope",
+          },
+          symbols = {
+            modified="*",
+          }
+        }
+      }
+    },
     extentions = {"oil", "mason", "lazy", "trouble" }
   })
   end
