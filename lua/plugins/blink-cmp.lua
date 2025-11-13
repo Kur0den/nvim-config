a03fb8d03cf35f3faf05abc7839c87135cf06066
Kur0den <git@kur0den.net>
2025-11-13 09:59:06 +0900

    add blink-copilot

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,7 +1,16 @@
 return{
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    {
+      'fang2hou/blink-copilot',
+      opts = {
+        max_completions = 3,
+        max_attempts = 3,
+      }
+      }
+  },
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
@@ -43,7 +52,15 @@ return{
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'copilot', 'path', 'snippets', 'buffer' },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          score_offset = 100,
+          async = true,
+        }
+      }
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
