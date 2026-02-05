e1dede706ff4cf42db86808260570c64de779946
Kur0den <git@kur0den.net>
2026-02-05 17:43:40 +0900

    lspの設定を記述するファイルを追加

---
+++ lua/lsp.lua
@@ -0,0 +1,30 @@
+vim.lsp.config["ts_ls"] = {
+  settings = {
+    typescript = {
+      inlayHints = {
+        -- You can set this to 'all' or 'literals' to enable more hints
+        includeInlayParameterNameHints = "none", -- 'none' | 'literals' | 'all'
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        -- You can set this to 'all' or 'literals' to enable more hints
+        includeInlayParameterNameHints = "none", -- 'none' | 'literals' | 'all'
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayVariableTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  }
+}
