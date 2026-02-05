6e0b9ae111841662c12768acf9819243786646ac
Kur0den <git@kur0den.net>
2026-02-05 17:44:26 +0900

    lsp.luaが読み込まれるように

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require("options")
 require("filetypes")
 require("keymaps")
 require("functions")
+require("lsp")
 require("config.lazy")
 
 vim.opt.rtp:append(vim.fn.stdpath("config") .. "/lua/scripts")
