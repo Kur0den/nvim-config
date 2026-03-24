6b494bda8d7679f3797ede54d814823cf2c20ca5
Kur0den <git@kur0den.net>
2026-03-24 18:11:54 +0900

    image.luaをwindowsだとロードしないように調整

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,7 +1,11 @@
-return {
-  "3rd/image.nvim",
-  build = false,
-  opts = {
-    processor = "magick_cli",
+if vim.fn.has("win32") == 1 then
+  return {}
+else
+  return {
+    "3rd/image.nvim",
+    build = false,
+    opts = {
+      processor = "magick_cli",
+    }
   }
-}
+end
