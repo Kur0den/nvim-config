24e35d02ff57e982650e01edf1d38b70c198cab1
Kur0den <git@kur0den.net>
2026-05-25 02:49:15 +0900

    blink-copilotの設定がまだ残っていたので削除

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -45,14 +45,14 @@ return{
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'copilot', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
       providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-copilot",
-          score_offset = 100,
-          async = true,
-        }
+        -- copilot = {
+        --   name = "copilot",
+        --   module = "blink-copilot",
+        --   score_offset = 100,
+        --   async = true,
+        -- }
       }
     },
 
