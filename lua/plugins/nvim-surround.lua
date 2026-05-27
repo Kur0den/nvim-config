5b9dc3d3ef31d6f9022db2674788292e6c496459
Kur0den <git@kur0den.net>
2026-05-27 18:12:32 +0900

    add nvim-surround

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,12 @@
+return {
+    "kylechui/nvim-surround",
+    version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"}
+    -- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+    -- config = function()
+    --     require("nvim-surround").setup({
+    --         -- Put your configuration here
+    --     })
+    -- end
+}
