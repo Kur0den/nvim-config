057647dc0712b48b67ac2a676c48a829ed3096f5
Kur0den <git@kur0den.net>
2026-05-27 13:12:54 +0900

    add barbecue.nvim

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,13 @@
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+  },
+  opts = {
+    theme = {
+      separator = { fg = "#7AA682" }
+    }
+  }
+}
