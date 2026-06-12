27f8027c70a623ab07bc0375d3bbacecc7dc8c21
Kur0den <git@kur0den.net>
2026-06-12 10:35:33 +0900

    moltenの設定を調整

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,12 +16,27 @@ vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>",
     { silent = true, desc = "Initialize the plugin" })
 vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>",
     { silent = true, desc = "run operator selection" })
-vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>",
+vim.keymap.set("n", "<localleader>mrl", ":MoltenEvaluateLine<CR>",
     { silent = true, desc = "evaluate line" })
-vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>",
+vim.keymap.set("n", "<localleader>mrr", ":MoltenReevaluateCell<CR>",
     { silent = true, desc = "re-evaluate cell" })
-vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
+vim.keymap.set("v", "<localleader>mr", ":<C-u>MoltenEvaluateVisual<CR>gv",
     { silent = true, desc = "evaluate visual selection" })
+vim.keymap.set("n", "<localleader>me", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
+vim.keymap.set("n", "<localleader>mos", ":noautocmd MoltenEnterOutput<CR>", { desc = "open output window", silent = true })
+vim.keymap.set("n", "<localleader>moh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
+vim.keymap.set("n", "<localleader>mmd", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
+
+-- quarto
+local runner = require("quarto.runner")
+vim.keymap.set("n", "<localleader>rc", runner.run_cell,  { desc = "run cell", silent = true })
+vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+vim.keymap.set("n", "<localleader>rA", runner.run_all,   { desc = "run all cells", silent = true })
+vim.keymap.set("n", "<localleader>rl", runner.run_line,  { desc = "run line", silent = true })
+vim.keymap.set("v", "<localleader>r",  runner.run_range, { desc = "run visual range", silent = true })
+vim.keymap.set("n", "<localleader>RA", function()
+  runner.run_all(true)
+end, { desc = "run all cells of all languages", silent = true })
 
 -- flutter
 -- vim.keymap.set("n", "<leader>f0", require("telescope").extensions.flutter.commands, { desc = "Open command Flutter" })
--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,9 +1,79 @@
 return {
+  {
     "benlubas/molten-nvim",
     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     build = ":UpdateRemotePlugins",
     init = function()
-        -- this is an example, not a default. Please see the readme for more configuration options
-        vim.g.molten_output_win_max_height = 12
+      -- this is an example, not a default. Please see the readme for more configuration options
+      -- vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 12
+      -- I find auto open annoying, keep in mind setting this option will require setting
+      -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
+      vim.g.molten_auto_open_output = false
+
+      -- this guide will be using image.nvim
+      -- Don't forget to setup and install the plugin if you want to view image outputs
+      -- vim.g.molten_image_provider = "image.nvim"
+
+      -- optional, I like wrapping. works for virt text and the output window
+      vim.g.molten_wrap_output = true
+
+      -- Output as virtual text. Allows outputs to always be shown, works with images, but can
+      -- be buggy with longer images
+      vim.g.molten_virt_text_output = true
+
+      -- this will make it so the output shows up below the \`\`\` cell delimiter
+      vim.g.molten_virt_lines_off_by_1 = true
     end,
+  },
+  {
+    "GCBallesteros/jupytext.nvim",
+    opts = {
+      style = "markdown",
+      output_extension = "md",
+      force_ft = "markdown"
+    }
+  },
+  {
+    "quarto-dev/quarto-nvim",
+    dependencies = {
+      "jmbuhr/otter.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = {
+      "quatro",
+      "markdown"
+    },
+    opts = {
+      debug = false,
+      closePreviewOnExit = true,
+      lspFeatures = {
+        enabled = true,
+        chunks = "curly",
+        languages = { "r", "python", "julia", "bash", "html" },
+        diagnostics = {
+          enabled = true,
+          triggers = { "BufWritePost" },
+        },
+        completion = {
+          enabled = true,
+        },
+      },
+      keymap = {
+        -- NOTE: setup your own keymaps:
+        hover = "H",
+        definition = "gd",
+        rename = "<leader>rn",
+        references = "gr",
+        format = "<leader>gf",
+      },
+      codeRunner = {
+        enabled = true,
+        default_method = "molten", -- "molten", "slime", "iron" or <function>
+        ft_runners = { python = "molten"}, -- filetype to runner, ie. `{ python = "molten" }`.
+        -- Takes precedence over `default_method`
+        never_run = { 'yaml' }, -- filetypes which are never sent to a code runner
+      },
+    }
+  }
 }
