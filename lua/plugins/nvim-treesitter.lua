4e045b6399a41c9ca77c686e646cff15c51647f4
Kur0den <git@kur0den.net>
2026-06-15 23:21:03 +0900

    treesitterが動くように設定を調整

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,25 +3,34 @@ return {
   branch = 'main',
   lazy = false,
   build = ":TSUpdate",
-  opts = {
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-    ensure_installed = {
-      "python",
-      "lua",
-      "rust",
-      "javascript",
-      "typescript",
-      "html",
-      "css",
-      "json",
-      "markdown",
-      "markdown_inline",
-    },
-
-  }
+  -- opts = {
+  --   highlight = {
+  --     enable = true,
+  --   },
+  --   indent = {
+  --     enable = true,
+  --   },
+  --   ensure_installed = {
+  --     "python",
+  --     "lua",
+  --     "rust",
+  --     "javascript",
+  --     "typescript",
+  --     "html",
+  --     "css",
+  --     "json",
+  --     "markdown",
+  --     "markdown_inline",
+  --   },
+  -- }
+  config = function ()
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+      callback = function ()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end
+    
 }
